0b8c117d3f0879ffbe6f21f2b9fd7c38cb2e411e
Alejandro Pizarroso Rodríguez <77840712+apizarroso1@users.noreply.github.com>
2022-05-18 21:01:19 +0200

    Delete relacionesJPA directory

--- relacionesJPA/src/main/java/org/dam2/relacionesJPA/repositorio/GrupoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package org.dam2.relacionesJPA.repositorio;
-
-import org.dam2.relacionesJPA.modelo.Grupo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GrupoRepositorio extends CrudRepository<Grupo, String> {
-
-}
--- relacionesJPA/src/main/java/org/dam2/relacionesJPA/repositorio/ModuloRepositorio.java
+++
@@ -1,8 +0,0 @@
-package org.dam2.relacionesJPA.repositorio;
-
-import org.dam2.relacionesJPA.modelo.Modulo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ModuloRepositorio extends CrudRepository<Modulo, String> {
-
-}
